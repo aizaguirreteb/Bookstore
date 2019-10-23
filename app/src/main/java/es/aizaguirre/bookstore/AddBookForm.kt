8af80ab644098ae2de7f2df45fd583bd336c36a6
Alejandra Izaguirre <aleizatebar@gmail.com>
2019-10-23 14:14:36 +0200

    Initial Commit

---
+++ app/src/main/java/es/aizaguirre/bookstore/AddBookForm.kt
@@ -0,0 +1,12 @@
+package es.aizaguirre.bookstore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddBookForm : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_book_form)
+    }
+}
