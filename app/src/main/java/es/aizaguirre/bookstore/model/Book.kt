507591ca4d45b03de4802ca8f10dd97bfd58815f
Alejandra Izaguirre <aleizatebar@gmail.com>
2019-10-24 14:06:48 +0200

    Model class Book (All Attributes)

--- app/src/main/java/es/aizaguirre/bookstore/model/Book.kt
+++ app/src/main/java/es/aizaguirre/bookstore/model/Book.kt
@@ -1,8 +1,17 @@
 package es.aizaguirre.bookstore.model
 
+import java.time.LocalDate
+
 data class Book (
-    val isbn : String,
     val title : String,
+    val cover : String,
+    val isbn : String,
+    val authors : String,
+    val editorial : String,
+    val binding : String,
+    val date : LocalDate,
+    val numberOfPages : Int,
+    val price : Double,
     val description : String
 ){
     override fun toString(): String {
