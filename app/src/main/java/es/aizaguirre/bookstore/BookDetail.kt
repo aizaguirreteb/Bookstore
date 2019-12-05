fde0e66e8fd8a19f79f88b3756704d0e85ea3537
Alejandra Izaguirre <aleizatebar@gmail.com>
2019-12-05 10:08:47 +0100

    Activity showing detail of book refactored to Fragment

--- app/src/main/java/es/aizaguirre/bookstore/BookDetail.kt
+++ app/src/main/java/es/aizaguirre/bookstore/BookDetail.kt
@@ -2,25 +2,34 @@ package es.aizaguirre.bookstore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import es.aizaguirre.bookstore.model.Book
 import es.aizaguirre.bookstore.model.Catalog
 import kotlinx.android.synthetic.main.activity_book_detail.*
 
-class BookDetail : AppCompatActivity() {
+class BookDetail : Fragment() {
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.activity_book_detail, container, false)
+    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_book_detail)
-        var extra = intent.extras
-        var book = extra?.get("ITEM_PULSADO") as Book
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        var book = arguments!!.get("ITEM_PULSADO") as Book
+
 
         txtBookTitle.text = book.title
         txtDescription.text = book.description
         txtAuthor.text = book.authors
         Glide
-            .with(baseContext)
+            .with(context!!)
             .load(book.cover)
             .centerCrop()
             .placeholder(R.drawable.placeholder)
@@ -28,4 +37,7 @@ class BookDetail : AppCompatActivity() {
 
 
     }
+
+
+
 }
